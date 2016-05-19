d9045d4eb9e46bc1f9cb0468cf7f4312a3b68307
rollerz <keith1437@gmail.com>
2016-05-18 23:57:56 -0500

    ver 3

--- main.py
+++ main.py
@@ -19,12 +19,17 @@ import jinja2
 import urllib
 import os
 import time
+from dateutil import relativedelta
 import datetime
+from pytz import timezone
+import pytz
+from tzlocal import get_localzone
+import tzlocal
 import models
 from google.appengine.ext import ndb
 
 Bill = models.Bill
-Account = models.Account
+User = models.User
 
 # IDEA: Use a json file to have a precreated set of bills and load them in a class
 
@@ -34,11 +39,16 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 #  End of imports
-# IDEA: The jinja2 temp is always index.html? Because Angular?
-# IDEA: write class that is run maybe always that checks bill dates and increments
 
 
 class NewBill(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'curUser': self.request.cookies.get('uName')
+        }
+        template = JINJA_ENVIRONMENT.get_template('temp/newbill-temp.html')
+        self.response.write(template.render(template_values))
+
     def post(self):
         name = self.request.get("bName")
         date = str(self.request.get("bDate"))
@@ -52,12 +62,12 @@ class NewBill(webapp2.RequestHandler):
         date = time.strptime(date, "%Y-%m-%d")
 
         finDate = datetime.date(year=date.tm_year, month=date.tm_mon, day=date.tm_mday)
-        newBill = Bill(bName=name, bDate=finDate, bRepeat=repeat, bAmount=fltamount)
+        newBill = Bill(bName=name, bDate=finDate, bRepeat=repeat, bAmount=fltamount, bUser=self.request.cookies.get('uName'), bPaid=False)
         newBill.put()
-        # TODO: Maybe check to see if bill name is taken, tho not indexed
 
         template_values = {
-            'bill': newBill
+            'bill': newBill,
+            'curUser': self.request.cookies.get('uName')
         }
 
         template = JINJA_ENVIRONMENT.get_template('temp/success-temp.html')
@@ -65,25 +75,288 @@ class NewBill(webapp2.RequestHandler):
 
 
 class Update(webapp2.RequestHandler):
-    def post(self):
-        template = JINJA_ENVIRONMENT.get_template('temp/success-temp.html')
-        self.response.write(template.render())
-
-
-class ListBills(webapp2.RequestHandler):
     def get(self):
-        template = JINJA_ENVIRONMENT.get_template('temp/bills.html')
-        self.response.write(template.render())
+        user = User.query(User.uName == self.request.cookies.get('uName')).fetch()[0]
+        template_values = {
+            'curUser': self.request.cookies.get('uName'),
+            'user': user
+        }
+        template = JINJA_ENVIRONMENT.get_template('temp/update-temp.html')
+        self.response.write(template.render(template_values))
+
+    def post(self):
+        user = User.query(User.uName == self.request.cookies.get('uName')).fetch()[0]
+        add = self.request.get("uAdd")
+        rem = self.request.get("uRem")
+        cha = self.request.get("uCha")
+        init = self.request.get("uInit")
+        template_values = {
+            'user': user,
+            'curUser': self.request.cookies.get('uName')
+        }
+
+        if add:
+            user.accAmount = user.accAmount + float(add)
+            user.put()
+            template_values.update({'succ': 'add', 'num': add, 'bList': Bill.query(Bill.bUser == user.uName).fetch()})
+        if rem:
+            if user.accAmount > float(rem):
+                user.accAmount = user.accAmount - float(rem)
+                user.put()
+                template_values.update({'succ': 'rem', 'num': rem})
+            else:
+                template_values.update({'error': 'fundFail', 'num': rem})
+        if cha:
+            user.accAmount = float(cha)
+            user.put()
+            template_values.update({'succ': 'cha', 'num': cha})
+        if init:
+            user.accAmount = float(init)
+            user.put()
+            template_values.update({'succ': 'init', 'num': init})
+
+        # updated bill list
+        bList = Bill.query(Bill.bUser == self.request.cookies.get('uName')).fetch()
+        unpaid = []
+
+        for b in bList:
+            if b.bPaid is False:
+                unpaid.append(b)
+        template_values.update({'bList': unpaid})
+
+        template = JINJA_ENVIRONMENT.get_template('temp/account-temp.html')
+        self.response.write(template.render(template_values))
+
+
+class Bills(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'bList': Bill.query(Bill.bUser == self.request.cookies.get('uName')).fetch(),
+            'curUser': self.request.cookies.get('uName')
+        }
+        template = JINJA_ENVIRONMENT.get_template('temp/bills-temp.html')
+        self.response.write(template.render(template_values))
+
+
+class BillDetail(webapp2.RequestHandler):
+    def get(self, para):
+        curBill = Bill.get_by_id(int(para))
+        template_values = {
+            'curUser': self.request.cookies.get('uName'),
+            'bill': curBill
+        }
+        template = JINJA_ENVIRONMENT.get_template('temp/bill-temp.html')
+        self.response.write(template.render(template_values))
+
+
+class PayBill(webapp2.RequestHandler):
+    def post(self, para):
+        curBill = Bill.get_by_id(int(para))
+        user = User.query(User.uName == self.request.cookies.get('uName')).fetch()[0]
+        bList = Bill.query(Bill.bUser == self.request.cookies.get('uName')).fetch()
+
+        if user.accAmount < curBill.bAmount:
+            template_values = {
+                'user': User.query(User.uName == self.request.cookies.get('uName')).fetch()[0],
+                'curUser': self.request.cookies.get('uName'),
+                'bill': curBill,
+                'error': 'insFail'
+            }
+
+        else:
+            user.accAmount = user.accAmount - curBill.bAmount
+            curDate = datetime.datetime.today().date()
+            curBill.bPayDate = curDate
+            curBill.bPaid = True
+            curBill.put()
+            user.put()
+            template_values = {
+                'curUser': self.request.cookies.get('uName'),
+                'user': user,
+                'billPaid': curBill
+            }
+            if curBill.bRepeat is not 'never':
+                if curBill.bRepeat == 'month':
+                    nextDate = curBill.bDate + relativedelta.relativedelta(months=1)
+
+                if curBill.bRepeat == 'week':
+                    nextDate = curBill.bDate + relativedelta.relativedelta(weeks=1)
+
+                if curBill.bRepeat == 'year':
+                    nextDate = curBill.bDate + relativedelta.relativedelta(years=1)
+
+                nextBill = Bill(bName=curBill.bName, bDate=nextDate, bRepeat=curBill.bRepeat, bAmount=curBill.bAmount, bUser=self.request.cookies.get('uName'), bPaid=False)
+                nextBill.put()
+                template_values.update({'next': nextBill})
+
+        # updated bill list
+        unpaid = []
+
+        for b in bList:
+            if b.bPaid is False:
+                unpaid.append(b)
+
+        template_values.update({'bList': unpaid})
+        template = JINJA_ENVIRONMENT.get_template('temp/account-temp.html')
+        self.response.write(template.render(template_values))
+
+
+class Register(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'curUser': self.request.cookies.get('uName')
+        }
+        template = JINJA_ENVIRONMENT.get_template('temp/register-temp.html')
+        self.response.write(template.render(template_values))
+
+    def post(self):
+        # TODO: Add more try blocks around any data forms
+        uName = self.request.get('user')
+        raw_password = self.request.get('pass')
+        confirm = self.request.get('conf')
+        userList = User.query().fetch()
+        userCheck = False
+        template_values = {}
+
+        if raw_password != confirm or len(raw_password) <= 7:
+            if len(raw_password) <= 7:
+                template_values.update({'user': uName, 'error': 'lenFail'})
+            else:
+                template_values.update({'user': uName, 'error': 'confFail'})
+
+        else:  # if passwords match, then if user already exists
+            if len(User.query(User.uName == uName).fetch(1)) == 0:
+                self.response.set_cookie('uName', uName, path="/")
+                newUser = User(uName=uName, password=raw_password)
+                newUser.put()
+                template_values.update({'user': newUser, 'succ': 'uReg'})
+                userCheck = True
+            else:
+                template_values.update({'user': uName, 'error': 'uDup'})
+
+        if userCheck:
+            template = JINJA_ENVIRONMENT.get_template('temp/account-temp.html')
+        else:
+            template = JINJA_ENVIRONMENT.get_template('temp/register-temp.html')
+
+        self.response.write(template.render(template_values))
+
+
+class Login(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'curUser': self.request.cookies.get('uName')
+        }
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+    def post(self):
+        uName = self.request.get('loginUser')
+        password = self.request.get('loginPass')
+        attUser = None
+        template_values = {}
+        login = False
+        passFail = False
+
+        try:
+            attUser = User.query(User.uName == uName).fetch()[0]
+
+        except IndexError:
+            if not User.query().fetch():
+                template_values.update({'error': 'noUsers'})
+            else:
+                template_values.update({'userFail': uName, 'error': 'uNotFound'})
+
+        else:
+            if attUser and attUser.password == password:  # success
+                self.response.set_cookie('uName', uName, path="/")
+                template_values.update({'user': attUser, 'succ': 'login', 'curUser': self.request.cookies.get('uName')})
+                login = True
+
+            else:  # fail
+                template_values.update({'userFail': uName, 'error': 'passFail'})
+                passFail = True
+
+        if login:
+            template = JINJA_ENVIRONMENT.get_template('temp/account-temp.html')
+        elif passFail:
+            template = JINJA_ENVIRONMENT.get_template('index.html')
+        else:
+            template = JINJA_ENVIRONMENT.get_template('temp/account-temp.html')
+        self.response.write(template.render(template_values))
+
+
+class LogOut(webapp2.RequestHandler):
+    def get(self):
+        curUser = self.request.cookies.get('uName')
+        tempUser = curUser  # used to display user for user logout
+
+        template_values = {
+            'user': tempUser
+        }
+        self.response.delete_cookie('uName')
+        template = JINJA_ENVIRONMENT.get_template('temp/logout-temp.html')
+        self.response.write(template.render(template_values))
+
+
+class Account(webapp2.RequestHandler):
+    def get(self):
+        user = User.query(User.uName == self.request.cookies.get('uName')).fetch()[0]
+        bList = Bill.query(Bill.bUser == self.request.cookies.get('uName')).fetch()
+        unpaid = []
+
+        for b in bList:
+            if b.bPaid is False:
+                unpaid.append(b)
+
+        template_values = {
+            'user': user,
+            'curUser': self.request.cookies.get('uName'),
+            'bList': unpaid
+        }
+        template = JINJA_ENVIRONMENT.get_template('temp/account-temp.html')
+        self.response.write(template.render(template_values))
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        today = datetime.datetime.now()
+        threeWks = today + datetime.timedelta(days=21)
+        bList = Bill.query(Bill.bUser == self.request.cookies.get('uName')).fetch()
+        upcBills = []
+        upcTotal = 0
+
+        # FIXME: no matter what happens, I cannot get the actual local time. its totally borked
+        # print "the tz is "
+        # print timezone('US/Central')
+        # print datetime.datetime.now(tz=get_localzone())
+
+        # generate unpaid bills for next 3 weeks
+        for b in bList:
+            if b.bDate < threeWks.date() and b.bDate >= today.date() and not b.bPaid:
+                upcTotal = b.bAmount + upcTotal
+                upcBills.append(b)
+
+        template_values = {
+            'curUser': self.request.cookies.get('uName'),
+            'upcoming': upcBills,
+            'sum': upcTotal,
+            'today': today.date()
+        }
+        if self.request.cookies.get('uName'):
+            template_values.update({'user': User.query(User.uName == self.request.cookies.get('uName')).fetch()[0]})
         template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render())
-# IDEA: Use webapp2 to load jinja2 templates from routeparams? ('#/bills', ListBills)?
+        self.response.write(template.render(template_values))
+
 app = webapp2.WSGIApplication([
-    ('/newBill', NewBill),
+    ('/newbill', NewBill),
     ('/update', Update),
-    ('/bills', ListBills),
+    ('/bills', Bills),
+    (r'/bill/(\w+)', BillDetail),
+    ('/reg', Register),
+    ('/login', Login),
+    ('/logout', LogOut),
+    (r'/pay/(\w+)', PayBill),
+    ('/account', Account),
     ('/', MainHandler)
 ], debug=True)
--- models.py
+++ models.py
@@ -1,15 +1,17 @@
 from google.appengine.ext import ndb
 
-# IDEA: Add a user model for login and stuff
-
 
 class Bill(ndb.Model):
     bName = ndb.StringProperty(indexed=False)
     bDate = ndb.DateProperty(indexed=False)
     bRepeat = ndb.StringProperty(indexed=False)
     bAmount = ndb.FloatProperty(indexed=False)
+    bUser = ndb.StringProperty(indexed=True)
+    bPaid = ndb.BooleanProperty(indexed=True)
+    bPayDate = ndb.DateProperty(indexed=False)
 
 
-class Account(ndb.Model):
+class User(ndb.Model):
+    uName = ndb.StringProperty(indexed=True)
+    password = ndb.StringProperty(indexed=False)
     accAmount = ndb.FloatProperty(indexed=False)
-    accBills = ndb.StructuredProperty(Bill, repeated=True)
